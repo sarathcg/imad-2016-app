7b4bcaeacdb9b4ac6c57a923fecccffb3ae4989e
sarathcg <sarathcg007@gmail.com>
2016-12-06 11:20:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,20 +4,85 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
+var articles={
+'article-one':{
+   title: 'Article One | Aniruth',
+   heading: 'Article One',
+   date: 'Sep 27, 2016',
+   content:`<p>This is the content for my first article.</p>`
+ },
+ 'article-two':{
+   title: 'Article Two | Aniruth',
+   heading: 'Article Two',
+   date: 'Sep 27, 2016',
+   content:`<p>This is the content for my second article.</p>`
+ },
+ 'article-three':{
+   title: 'Article Three | Aniruth',
+   heading: 'Article Three',
+   date: 'Sep 27, 2016',
+   content:`<p>This is the content for my third article.</p>`
+ }
+ };
+ function createTemplate (data) {
+     var title = data.title;
+     var date = data.date;
+     var heading = data.heading;
+     var content = data.content;
+     
+     var htmlTemplate = `
+     <html>
+       <head>
+           <title>
+               ${title}
+           </title>
+           <meta name="viewport" content="width=device-width, initial-scale=1" />
+           <link href="/ui/style.css" rel="stylesheet" />
+       </head> 
+       <body>
+           <div class="container">
+               <div>
+                   <a href="/">Home</a>
+               </div>
+               <hr/>
+               <h3>
+                   ${heading}
+               </h3>
+               <div>
+                   ${date}
+               </div>
+               <div>
+                 ${content}
+               </div>
+           </div>
+      </body>
+     </html>
+     `;
+     return htmlTemplate;
+ }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+var counter =0;
+app.get('/counter', function (req, res) {
+  counter+=1;
+  res.send(counter.toString());
 });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
-
+app.get('/:articlename', function (req, res) {
+var articlename = req.params.articlename;
+  res.send(createTemplate(articles[articlename]));
+});
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
