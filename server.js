bffb52471736e28bcec9ccb4b23b9bf0af962eb6
sarathcg <sarathcg007@gmail.com>
2016-12-06 10:41:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get('/ui/madi.png', function (req, res) {
    }) ;
 });
 */
-var pool= new Pool(config);
+/*var pool= new Pool(config);*/
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
