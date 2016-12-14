15b585f41d3919c2506f5326520da52933791619
sarathcg <sarathcg007@gmail.com>
2016-12-14 09:57:29 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,33 @@
 console.log('Loaded!');
-var elment=document.getElementById();
+var reg=document.getElementById('reg');
+reg.onclick() = function()
+{
+     var request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              if (request.status === 200) {
+                  alert('User created successfully');
+                  register.value = 'Registered!';
+              } else {
+                  alert('Could not register the user');
+                  register.value = 'Register';
+              }
+          }
+        };
+         
+        var username = document.getElementById('u2').value;
+        var password = document.getElementById('p2').value;
+        if (username.trim() === '' || password.trim() === '') {
+        alert("Username/Password field can't be left empty");
+        return;
+    }
+        request.open('POST', '/create-user', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        reg.value = 'Registering...';
+    };
+
+}
+};
+}
+
