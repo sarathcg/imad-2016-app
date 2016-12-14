57b1292b4ab6668c741398ce76713bbdce650a0f
sarathcg <sarathcg007@gmail.com>
2016-12-14 06:49:51 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,6 +61,17 @@ app.get('/counter', function (req, res) {
   res.send(counter.toString());
 });
 
+function hash(input,salt)
+{
+    var hashed= crypto.pbkdf25ync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input', function (req, res) {
+    var hashedstring = hash(req.params.input,'this-is-some-random-string');
+  res.send(hashedstring);
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
