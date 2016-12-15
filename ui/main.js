e2699c87b294f886cc064fee09d418b377f08e3e
sarathcg <sarathcg007@gmail.com>
2016-12-15 08:51:40 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,5 @@
 console.log('Loaded!');
 var reg=document.getElementById('reg');
-reg.value="rdf";
 reg.onclick = function()
 {
      var request = new XMLHttpRequest();
@@ -18,8 +17,6 @@ reg.onclick = function()
          
         var username = document.getElementById('u2').value;
         var password = document.getElementById('p2').value;
-        console.log(username);
-        console.log(password);
        
 
         if (username.trim() === '' || password.trim() === '') {
@@ -32,4 +29,35 @@ reg.onclick = function()
         reg.innerHTML = 'Registering...';
 };
 
+var log = document.getElementById('log');
+log.onclick = function()
+{
+     var request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              if (request.status === 200) {
+                  alert('Successfully logged in');
+                  log.innerHTML = 'logged!';
+              } else {
+                  alert('Could not register the user');
+                  log.innerHTML = 'Log In';
+              }
+          }
+        };
+         
+        var username = document.getElementById('u2').value;
+        var password = document.getElementById('p2').value;
+        console.log(username);
+        console.log(password);
+       
+
+        if (username.trim() === '' || password.trim() === '') {
+        alert("Enter login credentials");
+        return;
+    }
+        request.open('POST', 'http://sarathcg.imad.hasura-app.io/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        log.innerHTML = 'logging in...';
+};
 
