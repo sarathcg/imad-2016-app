72bc6fe098b72f0c5e59055d660d2af9570663dd
sarathcg <sarathcg007@gmail.com>
2016-12-16 07:05:02 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -78,7 +78,7 @@ function loadLoggedInUser(username)
     user.style.display='block';
     user.style.marginTop='16px';
  //   var logout = document.getElementById('logout');
-   user.innerHTML = '<b> '+username+' </b>';
+   user.innerHTML = '<b>'+username+' </b>';
    logout.style.display='block';
    
     
